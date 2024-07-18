c1e440f6df1d96e19fe2375fe1c30e662c640523
ptthao0801 <ptthao0801@gmail.com>
2024-07-18 14:48:41 +0700

    first cm

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Formik } from "formik";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+    const REGEX = {
+        email: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/
+    };
+
+    function handleValidate(values) {
+        const errors = {};
+        if (!values.email) {
+            errors.email = "Required";
+        } else if (!REGEX.email.test(values.email)) {
+            errors.email = "Invalid email address";
+        }
+        if (!values.password) {
+            errors.password = "Required";
+        }
+        return errors;
+    }
+
+    function handleSubmit(values) {
+        alert("Login in successfully!!!");
+    }
+
+    return (
+        <div>
+            <h1>Sign up</h1>
+            <Formik
+                initialValues={{ email: "", password: "" }}
+                validate={handleValidate}
+                onSubmit={handleSubmit}
+            >
+                {({ values, errors, handleChange, handleSubmit }) => (
+                    <form onSubmit={handleSubmit}>
+                        <div
+                            className={`custom-input ${errors.email ? "custom-input-error" : ""
+                                }`}
+                        >
+                            <label>Email</label>
+                            <input
+                                type="email"
+                                name="email"
+                                value={values.email}
+                                onChange={handleChange}
+                            />
+                            <p className="error">{errors.email}</p>
+                        </div>
+                        <div
+                            className={`custom-input ${errors.password ? "custom-input-error" : ""
+                                }`}
+                        >
+                            <label>Password</label>
+                            <input
+                                type="password"
+                                name="password"
+                                value={values.password}
+                                onChange={handleChange}
+                            />
+                            <p className="error">{errors.password}</p>
+                        </div>
+                        <button type="submit">Submit</button>
+                    </form>
+                )}
+            </Formik>
+        </div>
+    );
 }
-
-export default App;
